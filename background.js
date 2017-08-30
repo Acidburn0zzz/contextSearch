da5d2d8364ba2fe96f1a70fbbe23cf21861d5078
Olivier de Broqueville <olivier.debroqueville@gmail.com>
2017-08-31 00:04:52 +0200

    added functions to obtain favicons

--- background.js
+++ background.js
@@ -6,6 +6,40 @@ function onError(error) {
     console.log(`Error: ${error}`)
 }
 
+function getIconUrl(head) {
+  var links = head.getElementsByTagName("link");
+  console.log(links);
+  for (let link of links) {
+  	let rel = link.getAttribute("rel");
+  	if (rel === "icon" || rel === "shortcut icon") {
+    	return link.getAttribute("href");
+    }
+  }
+  return "";
+}
+
+function getFaviconUrl(url){
+    let urlParts = url.replace('http://','').replace('https://','').split(/[/?#]/);
+    let domain = urlParts[0];
+	let head, faviconUrl = "";
+	const req = new XMLHttpRequest();
+    req.responseType = "document";
+	req.open('GET', domain, true); 
+	req.send(null);
+	req.onreadystatechange = function() {
+  	if (req.readyState === XMLHttpRequest.DONE && req.status === 200) {
+      head = req.response.head;
+      console.log(head);
+      faviconUrl = getIconUrl(head);
+      if (faviconUrl.indexOf("/") === 0) {
+      	faviconUrl = url + faviconUrl;
+      }
+      console.log(faviconUrl);
+      return faviconUrl;
+	  }
+	}
+}
+
 // Create the context menu using the search engines listed above
 function updateContextMenu(changes, area) {
     browser.contextMenus.removeAll();
@@ -14,15 +48,24 @@ function updateContextMenu(changes, area) {
             searchEngines = sortAlphabetically(data);
             searchEnginesArray = [];
             var index = 0;
+            var strId, strTitle, url, faviconUrl = "";
             for (var se in searchEngines) {
-                var strId = index.toString();
-                var strTitle = searchEngines[se].name;
+                strId = index.toString();
+                strTitle = searchEngines[se].name;
+                url = searchEngines[se].url;
+                faviconUrl = getFaviconUrl(url); // I think I need to add 'then' here?    
+                if (typeof faviconUrl === "undefined") {
+                    faviconUrl = "https://s2.googleusercontent.com/s2/favicons?domain_url=" + url;
+                }
                 searchEnginesArray.push(se);
                 if (searchEngines[se].show) {
                     browser.contextMenus.create({
                         id: strId,
                         title: strTitle,
-                        contexts: ["selection"]
+                        contexts: ["selection"],
+                        icons: {
+                            18: faviconUrl
+                        }
                     });
                 }
                 index += 1;
@@ -38,13 +81,13 @@ browser.contextMenus.onClicked.addListener(function(info, tab) {
     var id = parseInt(info.menuItemId);
     
     // Prefer info.selectionText over selection received by content script for these lengths (more reliable)
-    if (info.selectionText.length < 150) {
-	    selection = info.selectionText;
+    if (info.selectionText.length < 150 || info.selectionText.length > 150) {
+	    selection = info.selectionText.trim();
     }
     if (searchEnginesArray[id] != "linkedin") {
-        searchString = (selection).replace(/ /g, "+");
+        searchString = (selection.trim()).replace(/ /g, "+");
     } else {
-        searchString = selection;
+        searchString = (selection).trim();
     }
     targetUrl = searchEngines[searchEnginesArray[id]].url + searchString;
     browser.tabs.create({
